3fc32d4b6fcd7365ce319332679ae74ac68ed518
arbreshazeqiri <61945677+arbreshazeqiri@users.noreply.github.com>
2021-07-08 16:10:23 +0200

    Profile updated

--- src/pdg/controllers/ProfileController.java
+++ src/pdg/controllers/ProfileController.java
@@ -1,10 +1,39 @@
 package pdg.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import pdg.utils.SessionManager;
+
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ProfileController extends ChildController {
+
+    @FXML
+    Label usernameLabel, fullnameLabel, emailLabel, winsLabel, scoreLabel;
+
+    @FXML
+    ImageView countryImage;
+
     @Override
     public void initialize(URL url, ResourceBundle bundle) {
+        usernameLabel.setText(SessionManager.user.getUsername());
+        fullnameLabel.setText(SessionManager.user.getFullName());
+        emailLabel.setText(SessionManager.user.getEmail());
+        winsLabel.setText("" + SessionManager.user.getNumberOfWins());
+        scoreLabel.setText("" + SessionManager.user.getScore());
+
+
+        File f;
+        if(SessionManager.user.getCountry() == "Kosovo"){
+            f = new File("src/pdg/resources/images/Kosovo.png");
+        }
+        else{
+            f = new File("src/pdg/resources/images/Albania.png");
+        }
+        countryImage.setImage(new Image(f.toURI().toString()));
     }
 }
